595a68aa4473dd144f5437f8b761b6789b189cf3
Prohor722 <prohor.b722@gmail.com>
2022-03-22 00:24:23 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Heder.css'
+import logo from '../../images/Logo.svg'
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <nav>
+            <img src={logo} alt="" />
+            <div id='nav-items'>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Orders</a>
+                <a href="/inventory">Invertory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
     );
 };
 
